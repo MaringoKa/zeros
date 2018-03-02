54ab5a156eaa4917fcac5ba00ae7a7cf2af09c28
MaringoKa <centrenrr@gmail.com>
2018-03-02 11:40:30 +0200

    //

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    // your implementation
+    var count = 0;
+    var n = 5;
+    while (number != 0) {
+        number = Math.floor(number / n);
+        count = count + number;
+    }
+    return count;
 }
